032f98c5bc199a6a34a6301f8d7e61617a2d1194
he_zhiyuan <hzy_offer20@163.com>
2019-09-05 10:47:21 +0800

    add offer_42.cpp 和为S的两个数字

---
+++ offer_42.cpp
@@ -0,0 +1,24 @@
+/*剑指offer —— 42 和为S的两个数字
+描述：输入一个递增排序的数组和一个数字S，在数组中查找两个数，
+使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
+*/
+
+class Solution {
+public:
+	vector<int> FindNumbersWithSum(vector<int> array, int sum) {
+
+		vector<int> res;
+		int n = array.size();
+		int i = 0, j = n - 1;
+		while (i < j) {
+			if (array[i] + array[j] == sum) {
+				res.push_back(array[i]);
+				res.push_back(array[j]);
+				break;//相差越远乘积越小
+			}
+			while (i < j && array[i] + array[j] > sum) --j;
+			while (i < j && array[i] + array[j] < sum) ++i;
+		}
+		return res;
+	}
+};
